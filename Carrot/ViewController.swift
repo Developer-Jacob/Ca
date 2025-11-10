9c7269ea86bfe8615b9a738c2e44ed4580c35927
Jacob <jacob@yonsei.ac.kr>
2025-11-10 09:07:06 +0900

    프로젝트 생성

---
+++ Carrot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carrot
+//
+//  Created by Jacob on 11/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
